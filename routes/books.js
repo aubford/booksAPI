0f38787f405d77c5fafeab3b51e6884379bfabc9
Aubrey Ford <musicaubrey@gmail.com>
2016-01-14 15:03:37 -0700

    complete

--- routes/books.js
+++ routes/books.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var unirest = require('unirest');
+require('dotenv').load();
+
+
+var apistring = "http://api.nytimes.com/svc/books/v3/lists/hardcover-fiction.json?api-key=" + process.env.NYT_API_KEY;
 
 router.get('/books',function(req,res){
-  res.render('books/index')
+  unirest.get(apistring).end(function(response){
+    res.render('books/index', {
+      books: response.body.results.books
+    })
+  });
 })
 
 router.get('/books/new',function(req,res){
@@ -14,7 +23,20 @@ router.get('/books/:id/edit',function(req,res){
 })
 
 router.get('/books/:id',function(req,res){
-  res.render('books/show', {book: req.params.id})
+  unirest.get(apistring).end(function(response){
+    response = response.body.results.books;
+    var thisbook;
+
+    response.forEach(function(b){
+      if (b.primary_isbn13 === req.params.id)
+        thisbook = b
+    })
+
+    console.log(thisbook)
+    res.render('books/show', {
+      book: thisbook
+    })
+  });
 })
 
 router.post('/books/:id/delete',function(req,res){
